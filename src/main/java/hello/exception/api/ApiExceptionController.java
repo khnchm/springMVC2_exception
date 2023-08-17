c7c9b664c8b79a4b03167d330d2f6534302381e5
biznet <biznet@naonsoft.com>
2023-08-17 16:36:47 +0900

    [springMVC2]API예외처리
     - ExceptionHandler

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -6,16 +6,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 public class ApiExceptionController {
 
-    @GetMapping("/api/members/{id}")
+    @GetMapping("/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id){
         if(id.equals("ex")){
             throw new RuntimeException("잘못된 사용자");
@@ -32,14 +31,19 @@ public class ApiExceptionController {
         return new MemberDto(id, "hello "+id);
     }
 
-    @GetMapping("/api/response-status-ex1")
+    @GetMapping("/response-status-ex1")
     public String responseStatusEx1(){
         throw new BadRequestException();
     }
 
-    @GetMapping("/api/response-status-ex2")
+    @GetMapping("/response-status-ex2")
     public String responseStatusEx2(){
         throw new ResponseStatusException(HttpStatus.NOT_FOUND,"error.bad", new IllegalArgumentException());
+    }
+
+    @GetMapping("/default-handler-ex")
+    public String defaultException(@RequestParam Integer data){
+        return "ok";
     }
 
 
